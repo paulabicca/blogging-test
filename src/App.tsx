8cbb427158d6507e5cc2c087b184c5557d3d7e8a
Paula Bicca <paula.bicca@gmail.com>
2024-07-25 12:07:38 -0300

    refactor: :lipstick: Create global style (first structure).

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-     <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <p>
+        Testing this <a href="#">page</a>
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
