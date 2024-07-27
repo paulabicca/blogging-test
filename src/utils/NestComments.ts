09bc0f6cec2972d39fcc763503f1d48545ca69a8
Paula Bicca <paula.bicca@gmail.com>
2024-07-27 11:49:51 -0300

    feat: :sparkles: Create file for nest comments.

---
+++ src/utils/NestComments.ts
@@ -0,0 +1,24 @@
+import { Comment } from '../dataType';
+
+export const NestComments = (comments: Comment[]) => {
+  const map = new Map<number, Comment>();
+
+  comments.forEach(comment => {
+    map.set(comment.id, { ...comment, replies: [] });
+  });
+
+  const tree: Comment[] = [];
+
+  comments.forEach(comment => {
+    if (comment.respondsTo) {
+      const parent = map.get(comment.respondsTo.id);
+      if (parent) {
+        parent.replies?.push(map.get(comment.id)!);
+      }
+    } else {
+      tree.push(map.get(comment.id)!);
+    }
+  });
+
+  return tree;
+};
